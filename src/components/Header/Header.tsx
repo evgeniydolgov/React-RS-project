91b49d1fefd34f7c6f399c606247a276146e2a47
Evgeniy Dolgov <amyr214519@gmail.com>
2023-03-13 18:01:41 +0300

    feat: done with header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import styles from "./Header.module.css";
+import reactImg from "../../assets/react.svg";
 
 export class Header extends Component {
   render() {
     return (
       <>
         <div className={styles.container}>
-          <div className={styles.header}>Header</div>
+          <div className={styles.header}>
+            <ul className={styles.header__list}>
+              <li>Main Page</li>
+              <li>About Us</li>
+            </ul>
+            <div className={styles.logo}>
+              <img src={reactImg} alt="react image" />
+            </div>
+          </div>
         </div>
       </>
     );
