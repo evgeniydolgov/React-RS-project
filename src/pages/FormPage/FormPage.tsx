2ec496a677568a2e6ff725aad0c9c6528ae6c503
Evgeniy Dolgov <amyr214519@gmail.com>
2023-03-29 12:38:57 +0300

    refactor: repair error message component

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -4,7 +4,6 @@ import { CreatedForm } from "../../components/CreatedForm";
 import { Card } from "../../components/Card";
 import { CarterCard } from "../../components/CardList/CardList";
 import { FormCreate } from "../../components/CreatedForm/CreatedForm";
-import { SubmitHandler, useForm } from "react-hook-form";
 
 export class FormPage extends React.Component {
   state = {
