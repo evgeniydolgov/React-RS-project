e4ae0198bf7ca0795ef988e3d4bb678be9dc20b3
Evgeniy Dolgov <amyr214519@gmail.com>
2023-04-09 14:10:31 +0300

    fix: fixed handlers file

--- src/pages/MainPage/MainPage.test.tsx
+++ src/pages/MainPage/MainPage.test.tsx
@@ -6,11 +6,7 @@ import { MainPage } from "./MainPage";
 describe("Testing Rick", () => {
   it(`should be possible to remove items from the cart`, async () => {
     const { findByText } = render(<MainPage />);
-    it("fetch and render Testing Rick", async () => {
-      expect(await findByText("Testing Rick")).toBeInTheDocument();
-    });
-    it("fetch and render Testing Morty", async () => {
-      expect(await findByText("Testing Morty")).toBeInTheDocument();
-    });
+    expect(await findByText("Testing Rick")).toBeInTheDocument();
+    expect(await findByText("Testing Morty")).toBeInTheDocument();
   });
 });
--- src/utils/helpMock.tsx
+++ src/utils/helpMock.tsx
@@ -5,30 +5,32 @@ const handlers = [
   rest.get(`https://rickandmortyapi.com/api/character/`, (req, res, ctx) =>
     res(
       ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          name: "Testing Rick",
-          status: "Alive",
-          species: "Human",
-          type: "",
-          gender: "Male",
-          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-          url: "https://rickandmortyapi.com/api/character/1",
-          created: "2017-11-04T18:48:46.250Z",
-        },
-        {
-          id: 2,
-          name: "Testing Morty",
-          status: "Alive",
-          species: "Human",
-          type: "",
-          gender: "Male",
-          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-          url: "https://rickandmortyapi.com/api/character/1",
-          created: "2017-11-04T18:48:46.250Z",
-        },
-      ]),
+      ctx.json({
+        results: [
+          {
+            id: 1,
+            name: "Testing Rick",
+            status: "Alive",
+            species: "Human",
+            type: "",
+            gender: "Male",
+            image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            url: "https://rickandmortyapi.com/api/character/1",
+            created: "2017-11-04T18:48:46.250Z",
+          },
+          {
+            id: 2,
+            name: "Testing Morty",
+            status: "Alive",
+            species: "Human",
+            type: "",
+            gender: "Male",
+            image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            url: "https://rickandmortyapi.com/api/character/1",
+            created: "2017-11-04T18:48:46.250Z",
+          },
+        ],
+      }),
     ),
   ),
 ];
