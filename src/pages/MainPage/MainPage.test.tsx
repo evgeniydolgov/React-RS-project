122e6a48ef28c2545f9c2aa900139b3679ba8db2
Evgeniy Dolgov <amyr214519@gmail.com>
2023-04-09 12:49:13 +0300

    fix: fixed URL tests

--- src/pages/MainPage/MainPage.test.tsx
+++ src/pages/MainPage/MainPage.test.tsx
@@ -3,43 +3,8 @@ import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
 import { MainPage } from "./MainPage";
 
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-
-const mockServer = setupServer();
 describe("Testing Rick", () => {
   it(`should be possible to remove items from the cart`, async () => {
-    mockServer.use(
-      rest.get(`https://rickandmortyapi.com/api/character/`, (req, res, ctx) =>
-        res(
-          ctx.status(200),
-          ctx.json([
-            {
-              id: 1,
-              name: "Testing Rick",
-              status: "Alive",
-              species: "Human",
-              type: "",
-              gender: "Male",
-              image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-              url: "https://rickandmortyapi.com/api/character/1",
-              created: "2017-11-04T18:48:46.250Z",
-            },
-            {
-              id: 2,
-              name: "Testing Morty",
-              status: "Alive",
-              species: "Human",
-              type: "",
-              gender: "Male",
-              image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-              url: "https://rickandmortyapi.com/api/character/1",
-              created: "2017-11-04T18:48:46.250Z",
-            },
-          ]),
-        ),
-      ),
-    );
     const { findByText } = render(<MainPage />);
     it("fetch and render Testing Rick", async () => {
       expect(await findByText("Testing Rick")).toBeInTheDocument();
--- src/utils/helpMock.tsx
+++ src/utils/helpMock.tsx
@@ -1,15 +1,36 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-export const mockServer = setupServer();
-
-export const mockEndpoint = (
-  endpoint,
-  { body, httpVerb = `get`, status = 200 },
-) => {
-  mockServer.use(
-    rest[httpVerb](endpoint, (req, res, ctx) =>
-      res(ctx.status(status), ctx.json(body)),
+const handlers = [
+  rest.get(`https://rickandmortyapi.com/api/character/`, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          name: "Testing Rick",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Male",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          url: "https://rickandmortyapi.com/api/character/1",
+          created: "2017-11-04T18:48:46.250Z",
+        },
+        {
+          id: 2,
+          name: "Testing Morty",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Male",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          url: "https://rickandmortyapi.com/api/character/1",
+          created: "2017-11-04T18:48:46.250Z",
+        },
+      ]),
     ),
-  );
-};
+  ),
+];
+
+export const mockServer = setupServer(...handlers);
